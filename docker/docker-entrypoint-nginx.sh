4052249975b6410657a86a650be616358b42d17a
布洛妮娅鸭鸭鸭 <1935494577@qq.com>
2026-08-11 14:34:06 +0800

    fix(media): drop duplicate CSP from frontend nginx, rely on Baota site config

--- docker/docker-entrypoint-nginx.sh
+++ docker/docker-entrypoint-nginx.sh
@@ -3,14 +3,9 @@ set -eu
 
 OSS_BUCKET="${OSS_BUCKET:-jnao-talent-ai}"
 OSS_ENDPOINT="${OSS_ENDPOINT:-oss-cn-beijing.aliyuncs.com}"
-OSS_CDN_DOMAIN="${OSS_CDN_DOMAIN:-}"
-OSS_MEDIA_SRC="'self' blob: data: https://${OSS_BUCKET}.${OSS_ENDPOINT}"
-if [ -n "$OSS_CDN_DOMAIN" ]; then
-  OSS_MEDIA_SRC="${OSS_MEDIA_SRC} https://${OSS_CDN_DOMAIN}"
-fi
-export OSS_BUCKET OSS_ENDPOINT OSS_MEDIA_SRC
+export OSS_BUCKET OSS_ENDPOINT
 
-envsubst '${OSS_BUCKET} ${OSS_ENDPOINT} ${OSS_MEDIA_SRC}' \
+envsubst '${OSS_BUCKET} ${OSS_ENDPOINT}' \
     < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
